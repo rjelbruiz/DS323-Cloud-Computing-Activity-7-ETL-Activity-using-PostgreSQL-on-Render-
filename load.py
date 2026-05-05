841b1b2d09806f56787eb01b5465803dbcee8876
Ross Jervin Lorenz Ruiz <rjelbruiz@users.noreply.github.com>
2026-05-05 12:01:24 +0800

    Update load.py

--- load.py
+++ load.py
@@ -3,10 +3,15 @@ from sqlalchemy import create_engine
 
 def load_to_production(db_url):
     engine = create_engine(db_url)
-    # Consolidate cleaned tables into the 'BIG TABLE' equivalent
-    jp = pd.read_sql("SELECT * FROM japan_sales_cleaned", engine)
-    # (Repeat for Myanmar...)
     
-    final_df = jp # Simplified for example
-    final_df.to_sql("fact_sales_consolidated", engine, if_exists='replace', index=False)
-    return "Load Complete"
+    # Combine both datasets into the "BIG TABLE"
+    jp = pd.read_sql("SELECT * FROM japan_cleaned", engine)
+    mm = pd.read_sql("SELECT * FROM myanmar_cleaned", engine)
+    
+    jp['country'] = 'Japan'
+    mm['country'] = 'Myanmar'
+    
+    big_table = pd.concat([jp, mm], ignore_index=True)
+    big_table.to_sql("fact_sales_consolidated", engine, if_exists='replace', index=False)
+    
+    return "Final Load Successful!"
